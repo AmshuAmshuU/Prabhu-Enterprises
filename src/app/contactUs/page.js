510da71202c3b3a51bda13d8a44aa3d181e50a99
HrudhayH <hrudhay.2003@gmail.com>
2025-09-02 18:24:51 +0530

    Added Contact Us hero section with background image

--- src/app/contactUs/page.js
+++ src/app/contactUs/page.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import Image from "next/image";
+import backgroundImage from "../assets/images/contactUs/BackGroundImage.png";
 
-const ContactUS = () => {
+export default function ContactUs() {
   return (
-    <div>ContactUS</div>
-  )
+    <section className="relative h-[400px] flex items-center justify-center">
+      <Image
+        src={backgroundImage}
+        alt="Background"
+        fill
+        className="object-cover brightness-50"
+        priority
+      />
+      <div className="relative z-10 text-center">
+        <h1 className="text-5xl font-bold text-white">Contact Us</h1>
+        <p className="mt-2 text-lg text-gray-200">
+          Home / <span className="text-yellow-400">Contact Us</span>
+        </p>
+      </div>
+    </section>
+  );
 }
-
-export default ContactUS
