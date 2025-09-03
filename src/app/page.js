46376f23e1dcb06d4cfa316798202d25b1566109
Lavanya <lavilavanya384@gmail.com>
2025-09-03 11:46:02 +0530

    Next commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Hero from "@/components/homePage/heroSection";
+import LogoSection from "@/components/homePage/LogosSection";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      < LogoSection />
     </>
   );
 }
